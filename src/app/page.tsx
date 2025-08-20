4484d3fc5ad9caa93bb12650cf51ae0332faa0ee
Ronald <ronaldzamoradev@gmail.com>
2025-08-20 17:20:31 -0500

    feat: add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home () {
   return (
     <main className='w-full h-screen flex flex-col justify-center items-center'>
       <h1>Hello world!</h1>
+      <Button>Press me!</Button>
     </main>
   )
 }
