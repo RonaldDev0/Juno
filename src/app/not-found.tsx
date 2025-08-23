b05b9d06ad50274cf4c693e9e9c5163ed06936eb
Ronald <ronaldzamoradev@gmail.com>
2025-08-22 21:46:44 -0500

    feat: not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,4 @@
-'use client'
-
-export default function ErrorPage() {
+export default function NotFoundPage() {
   return (
     <main className='w-full h-screen flex justify-center items-center'>
       <p>Sorry, something went wrong</p>
