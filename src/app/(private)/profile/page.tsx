91110a3b0db5f91bf20a7bff25f405825e41bc42
Ronald <ronaldzamoradev@gmail.com>
2025-08-28 23:28:47 -0500

    feat: add /settings & /profile

---
+++ src/app/(private)/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function ProfilePage () {
+  return (
+    <main className='w-full h-[90dvh] flex flex-col gap-6 justify-center items-center'>
+      <p>Profile page 🎉</p>
+    </main>
+  )
+}
---
+++ src/app/(private)/settings/page.tsx
@@ -0,0 +1,7 @@
+export default function SettingsPage () {
+  return (
+    <main className='w-full h-[90dvh] flex flex-col gap-6 justify-center items-center'>
+      <p>Settings page 🎉</p>
+    </main>
+  )
+}
